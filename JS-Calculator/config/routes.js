cbfd755508ea58539aa4bcf84ff0bc9318809941
vexleet <vex_malamov@abv.bg>
2018-07-12 14:57:45 +0300

    Added functionallity

--- JS-Calculator/config/routes.js
+++ JS-Calculator/config/routes.js
@@ -2,5 +2,6 @@ const homeController = require('./../controllers/home');
 
 module.exports = (app) => {
     app.get('/', homeController.indexGet);
+    app.post('/', homeController.indexPost);
 };
 
--- JS-Calculator/controllers/home.js
+++ JS-Calculator/controllers/home.js
@@ -1,6 +1,25 @@
+const Calculator = require('../models/Calculator');
+
 module.exports = {
     indexGet: (req, res) => {
         res.render('home/index');
     },
+    indexPost: (req, res) => {
+        let calculatorBody = req.body;
 
+        let calculatorParams = calculatorBody["calculator"];
+        let calculator = new Calculator();
+        calculator.leftOperand = Number(calculatorParams.leftOperand);
+        calculator.operator = calculatorParams.operator;
+        calculator.rightOperand = Number(calculatorParams.rightOperand);
+
+        if(isNaN(calculator.leftOperand) || isNaN(calculator.rightOperand)){
+            let error = calculator.alertIfNan();
+            res.render('home/index', {'calculator': calculator, 'errorForNumber': error})
+        }
+
+        let result = calculator.calculateResult();
+
+        res.render('home/index', {'calculator': calculator, 'result': result});
+    }
 };
